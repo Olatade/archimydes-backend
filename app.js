4219968b4fc39c721395dd5223ee217b2c80372b
Olatade <4tade01@gmail.com>
2021-07-21 10:01:40 +0100

    Add swagger informatino

--- app.js
+++ app.js
@@ -2,10 +2,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 var cors = require("cors");
 const UsersRoute = require('./src/routes/Users');
+require("dotenv").config();
+const swaggerJsDoc = require('swagger-jsDoc');
+const swaggerUI = require('swagger-ui-express');
 
 // create the express app
 const app = express();
 app.use(cors());
+
+// generate swagger information
+const swaggerOptions = {
+  definition: {
+    info: {
+      title: "Archimydes backend",
+      description: "Archimydes node challenge solution",
+      version: "1.0.0",
+    },
+    servers:[`${process.env.HOST_URL}:${process.env.PORT}`]
+  },
+  apis: ["./src/*.js"],
+  // apis: ["./docs/**/*.yaml"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 //connect to mongoDB
 const dbURI = 'mongodb+srv://general:archimydes@archimydes.uagqk.mongodb.net/archimydes?retryWrites=true&w=majority';
@@ -17,7 +37,7 @@ mongoose.connect(dbURI, {
 })
 .then( result => {
   console.log('connected to database');
-  app.listen(3003, console.log('listening on port 3003'))
+  app.listen(process.env.PORT, console.log('listening on port 3003'))
 })
 .catch( err => {
   console.log(err);
@@ -28,6 +48,7 @@ mongoose.connect(dbURI, {
 app.use(express.json()); 
 app.use(express.urlencoded()); //Parse URL-encoded bodies
 
+// Routes
 app.use('/users', UsersRoute);
 
 // home route
--- src/routes/Users.js
+++ src/routes/Users.js
@@ -4,10 +4,60 @@ const { addUser, getUser, getAllUsers, deleteUser, updateUser } = require('../co
 const { userValidationResult, validateAddUser, validateUpdateUser} = require('../middlewares/user-validator');
 const { checkIfExist} = require ('../middlewares/user');
 
+
+/**
+ * @swagger
+ * /users:
+ * get:
+ *  description: use to request all users
+ *  responses: 
+ *    '200':
+ *      description: A successful response
+ */
 router.get('/', getAllUsers);
+
+/**
+ * @swagger
+ * /users:
+ * post:
+ *  description: used to create a new user
+ *  responses: 
+ *    '201':
+ *      description: A successful response
+ */
 router.post('/', validateAddUser, userValidationResult, checkIfExist, addUser);
+
+/**
+ * @swagger
+ * /users/:id:
+ * get:
+ *  description: used to get a single user
+ *  responses: 
+ *    '200':
+ *      description: A successful response
+ */
 router.get('/:id', getUser);
+
+/**
+ * @swagger
+ * /users/:id:
+ * delete:
+ *  description: used to delete a user
+ *  responses: 
+ *    '200':
+ *      description: A successful response
+ */
 router.delete('/:id', deleteUser);
+
+/**
+ * @swagger
+ * /users/:id:
+ * patch:
+ *  description: used to update a user
+ *  responses: 
+ *    '200':
+ *      description: A successful response
+ */
 router.patch('/:id', validateUpdateUser, userValidationResult, updateUser);
 
 module.exports = router;
